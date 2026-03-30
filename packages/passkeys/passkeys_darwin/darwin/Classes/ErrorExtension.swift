8cfbb60e907dde8472e6e93fb300c4025ed47afb
DoanhTran-Sotatek <doanh.tran@sotatek.com>
2026-03-30 21:58:00 +0700

    check no credential by reason

--- packages/passkeys/passkeys_darwin/darwin/Classes/ErrorExtension.swift
+++ packages/passkeys/passkeys_darwin/darwin/Classes/ErrorExtension.swift
@@ -18,36 +18,40 @@ extension FlutterError: Error {
             code = "unknown"
             break
         case ASAuthorizationError.canceled:
-            //            if (error.localizedDescription.contains("No credentials available for login.")) {
-            //                code = "no-credentials-available"
-            //            } else {
-            //                code = "cancelled"
-            //            }
-            //            break
             let nsError = error as NSError
             
-            
-            
             let reason = nsError.userInfo[NSLocalizedFailureReasonErrorKey] as? String
-            // Log để debug (rất quan trọng)
-            print("---Authorization ERROR---")
-            print("reason:", reason ?? "nil")
-            print("domain:", nsError.domain)
-            print("code:", nsError.code)
-            print("localizedDescription:", nsError.localizedDescription)
-            print("failureReason:", nsError.userInfo[NSLocalizedFailureReasonErrorKey] ?? "nil")
-            print("userInfo:", nsError.userInfo)
-            print("---Authorization ERROR END---")
-            // 👉 iOS không phân biệt rõ cancel vs no credential
-            // nên phải infer dựa vào context
             
-            if let reason = nsError.userInfo[NSLocalizedFailureReasonErrorKey] as? String,
-               reason.lowercased().contains("credential") {
+            if (reason != nil) {
                 code = "no-credentials-available"
             } else {
                 code = "cancelled"
             }
             break
+//            let nsError = error as NSError
+//            
+//            
+//            
+//            let reason = nsError.userInfo[NSLocalizedFailureReasonErrorKey] as? String
+//            // Log để debug (rất quan trọng)
+//            print("---Authorization ERROR---")
+//            print("reason:", reason ?? "nil")
+//            print("domain:", nsError.domain)
+//            print("code:", nsError.code)
+//            print("localizedDescription:", nsError.localizedDescription)
+//            print("failureReason:", nsError.userInfo[NSLocalizedFailureReasonErrorKey] ?? "nil")
+//            print("userInfo:", nsError.userInfo)
+//            print("---Authorization ERROR END---")
+//            // 👉 iOS không phân biệt rõ cancel vs no credential
+//            // nên phải infer dựa vào context
+//            
+//            if let reason = nsError.userInfo[NSLocalizedFailureReasonErrorKey] as? String,
+//               reason.lowercased().contains("credential") {
+//                code = "no-credentials-available"
+//            } else {
+//                code = "cancelled"
+//            }
+//            break
         case ASAuthorizationError.invalidResponse:
             code = "invalidResponse"
             break
