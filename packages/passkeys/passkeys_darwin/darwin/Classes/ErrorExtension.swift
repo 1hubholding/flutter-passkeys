9924284845b6b1869a71a70fbbbc8021f60d5d72
DoanhTran-Sotatek <doanh.tran@sotatek.com>
2026-03-30 19:29:17 +0700

    add log and fix no credential in ios

--- packages/passkeys/passkeys_darwin/darwin/Classes/ErrorExtension.swift
+++ packages/passkeys/passkeys_darwin/darwin/Classes/ErrorExtension.swift
@@ -18,7 +18,31 @@ extension FlutterError: Error {
             code = "unknown"
             break
         case ASAuthorizationError.canceled:
-            if (error.localizedDescription.contains("No credentials available for login.")) {
+            //            if (error.localizedDescription.contains("No credentials available for login.")) {
+            //                code = "no-credentials-available"
+            //            } else {
+            //                code = "cancelled"
+            //            }
+            //            break
+            let nsError = error as NSError
+            
+            
+            
+            let reason = nsError.userInfo[NSLocalizedFailureReasonErrorKey] as? String
+            // Log để debug (rất quan trọng)
+            print("---Authorization ERROR---")
+            print("reason:", reason ?? "nil")
+            print("domain:", nsError.domain)
+            print("code:", nsError.code)
+            print("localizedDescription:", nsError.localizedDescription)
+            print("failureReason:", nsError.userInfo[NSLocalizedFailureReasonErrorKey] ?? "nil")
+            print("userInfo:", nsError.userInfo)
+            print("---Authorization ERROR END---")
+            // 👉 iOS không phân biệt rõ cancel vs no credential
+            // nên phải infer dựa vào context
+            
+            if let reason = nsError.userInfo[NSLocalizedFailureReasonErrorKey] as? String,
+               reason.lowercased().contains("credential") {
                 code = "no-credentials-available"
             } else {
                 code = "cancelled"
@@ -41,7 +65,7 @@ extension FlutterError: Error {
             code = "unknown"
             break
         }
-
+        
         self.init(code: code, message: error.localizedDescription, details: "")
     }
     
